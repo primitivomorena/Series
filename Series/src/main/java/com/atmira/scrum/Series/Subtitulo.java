e64532c3ff216c1aa7779aa552040d32a7d74023
Laura <laura.rios1898@gmail.com>
2019-05-31 12:03:03 +0200

    Métodos de Capitulo y Subtitulo

--- Series/src/main/java/com/atmira/scrum/Series/Subtitulo.java
+++ Series/src/main/java/com/atmira/scrum/Series/Subtitulo.java
@@ -2,11 +2,17 @@ package com.atmira.scrum.Series;
 
 public class Subtitulo {
 
-	public static enum idioma {
+	public static enum Idioma {
 		ESPANOL, LATINO, FRANCES, CATALAN, EUSKERA, INGLES, ALEMAN
 	}
 
 	private String autor;
+	private Idioma idioma;
+
+	public Subtitulo(String autor, Idioma idioma) {
+		this.idioma = idioma;
+		this.autor = autor;
+	}
 
 	public String getAutor() {
 		return autor;
